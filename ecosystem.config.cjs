2d868614bd55e515a8be6833c466055bc90bf1e5
Beenlord <tombeenlord@gmail.com>
2024-07-03 00:52:22 +0300

    Update ecosystem.config.cjs

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
     apps: [
         {
             name: 'BlackProject',
-            port: '%port%',
+            port: '80',
             exec_mode: 'cluster',
             instances: '1',
             script: './.output/server/index.mjs',
