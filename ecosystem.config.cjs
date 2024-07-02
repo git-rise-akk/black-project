bb5cffd0bb6c512a7d77072de4fc7f79e8e967a3
Beenlord <tombeenlord@gmail.com>
2024-07-03 00:50:15 +0300

    Update ecosystem.config.cjs

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -6,10 +6,10 @@ module.exports = {
             exec_mode: 'cluster',
             instances: '1',
             script: './.output/server/index.mjs',
-            env: {
-                PORT: '80',
-                HOST: '0.0.0.0',
-            },
+            // env: {
+            //     PORT: '80',
+            //     HOST: '0.0.0.0',
+            // },
         }
     ]
 }
