4cc273eb34bd25d40b73186da0354be8188b9400
Beenlord <tombeenlord@gmail.com>
2024-07-03 00:14:38 +0300

    Reformat code

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,36 +1,33 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: false },
-  app: {
-    head: {
-      title: "Black Project",
-      htmlAttrs: {
-          lang: "ru",
-      },
-      charset: "utf-8",
+    devtools: {enabled: false},
+    app: {
+        head: {
+            title: "Black Project",
+            htmlAttrs: {
+                lang: "ru",
+            },
+            charset: "utf-8",
+        },
     },
-  },
-
-  modules: [
-    '@pinia/nuxt',
-    'nuxt-icons',
-    '@nuxt/image',
-    '@nuxtjs/device',
-  ],
-  css: ["@/assets/css/main.scss"],
-  vite: {
-    css: {
-        preprocessorOptions: {
-            scss: {
-                additionalData:
-                    '@import "@/assets/css/mixins.scss";',
+    modules: [
+        '@pinia/nuxt',
+        'nuxt-icons',
+        '@nuxt/image',
+        '@nuxtjs/device',
+    ],
+    css: ["@/assets/css/main.scss"],
+    vite: {
+        css: {
+            preprocessorOptions: {
+                scss: {
+                    additionalData:
+                        '@import "@/assets/css/mixins.scss";',
+                },
             },
         },
     },
-  },
-
-  image: {
-      quality: 80,
-      format: ["webp"],
-  },
+    image: {
+        quality: 80,
+        format: ["webp"],
+    },
 })
