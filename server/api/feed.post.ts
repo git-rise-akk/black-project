acd64be3659ef2020a21670d85637e746b816374
Beenlord <tombeenlord@gmail.com>
2024-07-03 23:23:36 +0300

    Fix isDev api value

--- server/api/feed.post.ts
+++ server/api/feed.post.ts
@@ -7,9 +7,9 @@ export default defineEventHandler(async (event) => {
     const res = { ...responseBase };
     const body = await readBody(event);
 
-    if (body.isDev) {
-        if (body.status) res.status = body.status;
-        if (body.message) res.status = body.message;
+    if (body?.isDev) {
+        if (body?.status) res.status = body.status;
+        if (body?.message) res.status = body.message;
         return res;
     }
 
