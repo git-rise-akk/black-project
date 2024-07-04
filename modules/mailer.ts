dcac875eb45e5bdae4075722b7fa27ea9ca39673
Beenlord <tombeenlord@gmail.com>
2024-07-04 23:27:18 +0300

    Make send mail module

---
+++ modules/mailer.ts
@@ -0,0 +1,39 @@
+import nodemailer from 'nodemailer';
+
+export default function (opt: object) {
+    opt = {
+        host: 'smtp.yandex.ru',
+        port: 587,
+        user: null,
+        pass: null,
+        from: null,
+        to: null,
+        ...opt,
+    };
+
+    const transporter = nodemailer.createTransport({
+        host: opt.host,
+        port: opt.port,
+        secure: false,
+        auth: {
+            user: opt.user,
+            pass: opt.pass,
+        },
+    });
+
+    return (html: string = "", subject: string = "") => new Promise((resolve, reject) => {
+        if (!opt.from || !opt.to || !html) reject();
+        opt.from = opt?.from ?? opt?.user;
+
+        transporter.sendMail({
+            from: opt?.from,
+            to: opt.to,
+            subject, html,
+        }, (error: any, info: any) => {
+            if (error) {
+                reject(error);
+            }
+            resolve(info);
+        });
+    });
+}
--- server/api/feed.post.ts
+++ server/api/feed.post.ts
@@ -1,51 +1,27 @@
-import nodemailer from 'nodemailer';
+import mailer from '@/modules/mailer.ts';
 
-const responseBase = {
-    statusCode: 200,
-    statusMessage: "Ok!",
-};
-
-const transporter = nodemailer.createTransport({
-    service: 'yandex',
-    auth: {
-        type: 'login',
-        user: '',
-        pass: '',
-    },
+const yandexMail = mailer({
+    user: '',
+    pass: '',
+    to: '',
 });
 
-const send = (mailOptions: any) => {
-    return new Promise((resolve, reject) => {
-        transporter.sendMail(mailOptions, (error: any, info: any) => {
-            if (error) {
-                reject(error)
-            }
-            resolve(info)
-        })
-
-    })
-}
-
 export default defineEventHandler(async (event) => {
-    const res = { ...responseBase };
-    const body = await readBody(event);
 
-    if (body?.isDev) {
-        if (body?.status) res.statusCode = body.status;
-        if (body?.message) res.statusMessage = body.message;
-        return res;
+    try {
+
+        const body = await readBody(event);
+        await yandexMail(body?.text, body?.title ?? "Untitled message");
+
+    } catch (err) {
+        return {
+            status: false,
+            message: 'Произошла ошибка!',
+        };
     }
 
-    // await send({
-    //     from: '',
-    //     to: '',
-    //     subject: 'Hello World!',
-    //     html: `<h1>Hello?</h1><p>How are you?</p>`,
-    // });
-
-    res.dot = {
-        ...process.env.EMAIL_USER,
+    return {
+        status: true,
+        message: 'Письмо отправлено!',
     };
-
-    return res;
 });
